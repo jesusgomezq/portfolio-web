cf95f22753f4c966e3fe76e0881795fa06b7ec5f
jesus <jgomez4543@gmail.com>
2025-02-05 21:24:50 -0300

    deploy

--- src/components/Projects/ProjectsCard.jsx
+++ src/components/Projects/ProjectsCard.jsx
@@ -8,13 +8,13 @@ import { SiVitest } from "react-icons/si";
 const ProjectsCard = ({ title, des, src }) => {
   return (
     <section
-      className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowAll
+      className="w-full p-4 lgl:px-10 h-auto lgl:py-10 rounded-lg shadow-shadowAll
     flex flex-col bg-gradient-to-r from-bodyColors to-[#202327] group hover:bg-gradient-to-b
     hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full overflow-hidden rounded-lg h-[80%]">
         <img
           src={src}
-          className="w-full h-72 object-cover lgl:group-hover:scale-110 duration-300"
+          className="w-full h-full object-cover lgl:group-hover:scale-110 duration-300"
           alt="src"
         />
       </div>
